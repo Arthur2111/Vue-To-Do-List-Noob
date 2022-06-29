ea5ac8f5e36e173a27cfd34291c649e5373dbbfd
WeiHwang <khong_weihwang@hotmail.com>
2022-06-29 19:29:30 +0800

    Default

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    data () {
+        return {
+            goals: [],
+            // added goals will be added into goals with a method
+            addedGoals: '',
+            visible: true,
+            caption: 'Hide'
+        }
+    },
+    methods: {
+        inputGoals() {
+            
+            this.goals.push(this.addedGoals)
+            this.addedGoals = ''
+            console.log('flag1')
+        },
+        toggleVisibility() {
+            this.visible= !this.visible
+            if (this.visible) {
+                this.caption= 'Hide'
+            } else {
+                this.caption = 'Show List'
+            }
+            console.log('flag2')
+        }
+    }
+
+})
+
+app.mount('#assignment')
